93a2fae0847e47b035b3dfefbbf3da887a539572
Shine0417 <b07902109@ntu.edu.tw>
2021-06-27 22:48:37 +0800

    add boundry

--- src/model/World.java
+++ src/model/World.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import characters.knight.Knight;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toSet;
 
@@ -22,6 +23,7 @@ public class World {
 
     public void update() {
         for (Sprite sprite : sprites) {
+            correctBoundary(sprite);
             sprite.update();
         }
     }
@@ -42,29 +44,50 @@ public class World {
 
     public void move(Sprite from, Dimension offset) {
         Point originalLocation = new Point(from.getLocation());
-        from.getLocation().translate(offset.width, offset.height);
+        if (checkBoundary(from, offset)) {
+            from.getLocation().translate(offset.width, offset.height);
 
-        Rectangle body = from.getBody();
-        // collision detection
-        for (Sprite to : sprites) {
-            if (to != from && body.intersects(to.getBody())) {
-                collisionHandler.handle(originalLocation, from, to);
+            Rectangle body = from.getBody();
+            // collision detection
+            for (Sprite to : sprites) {
+                if (to != from && body.intersects(to.getBody())) {
+                    collisionHandler.handle(originalLocation, from, to);
+                }
             }
         }
+
     }
 
     public Collection<Sprite> getSprites(Rectangle area) {
-        return sprites.stream()
-                .filter(s -> area.intersects(s.getBody()))
-                .collect(toSet());
+        return sprites.stream().filter(s -> area.intersects(s.getBody())).collect(toSet());
     }
 
     public List<Sprite> getSprites() {
         return sprites;
     }
 
-    // Actually, directly couple your model with the class "java.awt.Graphics" is not a good design
-    // If you want to decouple them, create an interface that encapsulates the variation of the Graphics.
+    private boolean checkBoundary(Sprite from, Dimension offset) {
+        if (from instanceof Knight) {
+            int leftBorder = (int) (from.getLocation().getX() + from.getBodySize().getWidth());
+            int rightBorder = (int) (from.getLocation().getX());
+            if (leftBorder + offset.width < 0 || rightBorder + offset.width > 394)
+                return false;
+        }
+        return true;
+    }
+
+    private void correctBoundary(Sprite sprite) {
+        if (sprite.getLocation().getX() + sprite.getBodySize().getWidth() < 0) {
+            sprite.setLocation(new Point(0, (int) sprite.getLocation().getY()));
+        } else if (sprite.getLocation().getX() > 394) {
+            sprite.setLocation(new Point(394, (int) sprite.getLocation().getY()));
+        }
+    }
+
+    // Actually, directly couple your model with the class "java.awt.Graphics" is
+    // not a good design
+    // If you want to decouple them, create an interface that encapsulates the
+    // variation of the Graphics.
     public void render(Graphics g) {
         for (Sprite sprite : sprites) {
             sprite.render(g);
